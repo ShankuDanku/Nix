07cf13a611365f96a117d079e16ebb97d2be11d2
Sashank Durbha <sashankdurbha@gmail.com>
2024-09-25 00:19:52 +0530

    directly installing alejandra as nix pkg

--- packages.nix
+++ packages.nix
@@ -1,11 +1,4 @@
-{pkgs, ...}: let
-  alejandra =
-    (import (builtins.fetchTarball {
-      url = "https://github.com/kamadorueda/alejandra/tarball/3.0.0";
-      sha256 = "18jm0d5xrxk38hw5sa470zgfz9xzdcyaskjhgjwhnmzd5fgacny4";
-    }) {})
-    .outPath;
-in {
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     # terminal
     kitty
