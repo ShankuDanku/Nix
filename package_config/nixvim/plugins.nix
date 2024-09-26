71d9cdf303ff06c868ea3ec29c3995f772fce4c9
Sashank Durbha <sashankdurbha@gmail.com>
2024-09-27 01:50:22 +0530

    added pylsp jedi plugins

--- package_config/nixvim/plugins.nix
+++ package_config/nixvim/plugins.nix
@@ -34,7 +34,16 @@
           };
           pylsp = {
             enable = true;
-            settings.plugins.ruff.enabled = true;
+            settings.plugins = {
+              ruff.enabled = true;
+              jedi_completion.enabled = true;
+              jedi_definition.enabled = true;
+              jedi_hover.enabled = true;
+              jedi_references.enabled = true;
+              jedi_signature_help.enabled = true;
+              jedi_symbols.enabled = true;
+              pylsp_mypy.enabled = true;
+            };
           };
         };
       };
